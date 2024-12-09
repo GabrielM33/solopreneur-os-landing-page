b3bec002f1ba086394ddd1e8a3fbf8bc26594234
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-09 09:42:47 -0300

    Update Footer component: change logo text to 'SolopreneurOS' and clean up formatting

--- components/footer.tsx
+++ components/footer.tsx
@@ -31,20 +31,21 @@ const footerLinks = [
   },
 ];
 
+
 export const Footer = () => (
   <footer className="mt-12 border-t border-transparent-white py-12 text-sm">
     <Container className="flex flex-col justify-between lg:flex-row">
       <div className="flex flex-col">
         <div className="flex items-center text-grey">
-          <Logo className="mr-4 h-4 w-4" /> 
-          <span>Envision</span>
+          <Logo className="mr-4 h-4 w-4" />
+          <span>SolopreneurOS</span>
         </div>
         <div className="mt-4 flex space-x-4 text-grey">
           <TwitterIcon />
           <GithubIcon />
         </div>
       </div>
-      
+     
       <div className="flex flex-wrap">
         {footerLinks.map((column) => (
           <div
