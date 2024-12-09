f5a87aaa7a81072400361d699ddd63a55e676a9e
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-09 09:45:07 -0300

    Refactor HomepageHero component: enhance structure, update button links, and improve text clarity

--- components/sections/homepage-hero.tsx
+++ components/sections/homepage-hero.tsx
@@ -1,36 +1,39 @@
 import { Button, Highlight } from "../button";
 import { Hero, HeroTitle, HeroSubtitle } from "../hero";
 import { HeroImage } from "../hero-image";
-import { ChevronIcon } from "../icons/chevron";
 
-export const HomepageHero = () => (
-  <Hero>
-    <Button
-      className="translate-y-[-1rem] animate-fade-in opacity-0"
-      href="/"
-      variant="secondary"
-      size="small"
-    >
-      <span>Save 30% - Launch Offer</span>{" "}
-    </Button>
-    <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms] font-semibold">
-      Solopreneur OS
-      <br className="hidden md:block" /> 
-    </HeroTitle>
-    <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-    Transform your business operations into seamless <br /> workflows that run on autopilot.
-    </HeroSubtitle>
-    <Button
-      className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:600ms]"
-      href="https://buy.stripe.com/dR6bMFdYp3b6aJiaEF"
-      variant="primary"
-      size="large"
-    >
-        <span>Get Instant Access </span>
-    </Button>
-    <div className="-mt-16">
-       <HeroImage />
-    </div>
-   
-  </Hero>
-);
+export const HomepageHero = () => {
+  return (
+    <Hero>
+      <div className="py-16">
+        <Button
+          className="translate-y-[-1rem] animate-fade-in opacity-0"
+          href="/"
+          variant="secondary"
+          size="small"
+        >
+          <span>Save 30% - Launch Offer</span>{" "}
+        </Button>
+        <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms] font-semibold">
+          SolopreneurOS
+          <br className="hidden md:block" />
+        </HeroTitle>
+        <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms] pb-10">
+          Transform your business operations into a seamless <br /> system that runs on autopilot.
+        </HeroSubtitle>
+        <Button
+          className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:600ms] -pb-10"
+          href="#pricing"
+          variant="primary"
+          size="large"
+        >
+          <span>Get Instant Access </span>
+        </Button>
+      </div>
+      <div className="-mt-16 -pb-36">
+        <HeroImage />
+      </div>
+    </Hero>
+  );
+};
+
