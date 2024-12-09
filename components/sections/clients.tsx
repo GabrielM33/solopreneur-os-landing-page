bb7ee611a7604e53572428e87e6513f3a5686910
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-09 09:47:24 -0300

    Update Clients section: replace logos with book images, enhance styling, and add animation effects; update global styles for smooth scrolling and new utility classes

--- components/sections/clients.tsx
+++ components/sections/clients.tsx
@@ -1,38 +1,27 @@
-import { AlanLogo } from "../logos/alan";
-import { ArcLogo } from "../logos/arc";
-import { CashAppLogo } from "../logos/cashapp";
-import { DescriptLogo } from "../logos/descript";
-import { LoomLogo } from "../logos/loom";
-import { MercuryLogo } from "../logos/mercury";
-import { OpenSeaLogo } from "../logos/opensea";
-import { PitchLogo } from "../logos/pitch";
-import { RampLogo } from "../logos/ramp";
-import { RaycastLogo } from "../logos/raycast";
-import { RetoolLogo } from "../logos/retool";
-import { VercelLogo } from "../logos/vercel";
 
 export const Clients = () => (
   <>
-    <p className="mb-12 text-center text-lg text-white md:text-xl">
+    <p className="mb-12 text-center text-lg text-white md:text-xl pb-10">
       <span className="text-primary-text">
         Built on
       </span>
       <br/> proven frameworks.
     </p>
 
-    <div className="flex flex-wrap justify-around gap-x-6 gap-y-8 [&_svg]:max-w-[16rem] [&_svg]:basis-[calc(50%-12px)] md:[&_svg]:basis-[calc(16.66%-20px)]">
-      <RampLogo />
-      <LoomLogo className="hidden md:block" />
-      <VercelLogo />
-      <DescriptLogo className="hidden md:block" />
-      <CashAppLogo />
-      <RaycastLogo />
-      <MercuryLogo />
-      <RetoolLogo />
-      <AlanLogo className="hidden md:block" />
-      <ArcLogo className="hidden md:block" />
-      <OpenSeaLogo className="hidden md:block" />
-      <PitchLogo className="hidden md:block" />
+
+    <div className="flex flex-wrap justify-around gap-y-6 [&_svg]:max-w-[16rem] [&_svg]:basis-[calc(50%-12px)] md:[&_svg]:basis-[calc(16.66%-20px)]">
+      <div className="animate-glow">
+        <Image src={"/books/atomic-habits.jpg"} alt="Atomic Habits" width={100} height={100} className="rounded-lg shadow-glow" />
+      </div>
+      <div className="animate-glow [animation-delay:200ms]">
+        <Image src={"/books/e-myth.jpg"} alt="E-Myth" width={100} height={100} className="rounded-lg shadow-glow" />
+      </div>
+      <div className="animate-glow [animation-delay:400ms]">
+        <Image src={"/books/gtd.jpg"} alt="Getting Things Done" width={100} height={100} className="rounded-lg shadow-glow" />
+      </div>
+      <div className="animate-glow [animation-delay:600ms]">
+        <Image src={"/books/4-hour.jpg"} alt="Getting Things Done" width={100} height={100} className="rounded-lg shadow-glow" />
+      </div>
     </div>
   </>
 );
